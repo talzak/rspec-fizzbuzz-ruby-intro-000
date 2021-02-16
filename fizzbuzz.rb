399d88b21eb3da8ff1db8ec7a1a9d4fcae12339e
Tal Zak <tal.zak26@gmail.com>
2021-02-16 18:07:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(x)
+  y = x / 3
+  z = x / 5
   if y.class == Fixnum
-    y = x / 3
     return 'Fizz'
   elsif z.class == Fixnum
-    z = x / 5
     return 'Buzz'
   end
 end
